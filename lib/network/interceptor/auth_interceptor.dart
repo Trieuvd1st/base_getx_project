8b49c9ee0d3e4a7d9108c4fb522d3cd237bc7ab2
gingi <ductrieu98htrung@gmail.com>
2023-01-09 09:08:23 +0700

    git add git ignore

--- lib/network/interceptor/auth_interceptor.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-class AuthInterceptor extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers.addAll({"Authorization": auth.token});
-    handler.next(options);
-  }
-}
